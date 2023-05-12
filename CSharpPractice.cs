040b0bfc0424f261a78a6af3a4b214e3615858d3
Animeology <joseph.nguyen19@yahoo.com>
2023-05-12 18:34:50 -0500

    Hello World Exercise

---
+++ CSharpPractice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class CSharpPractice
+{
+    static void Main(string[] args)
+    {
+        HelloWorld();
+    }
+
+    static void HelloWorld()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
