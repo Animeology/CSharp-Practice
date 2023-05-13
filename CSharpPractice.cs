7dc294f242e33cd6718c9aa394498742a1659f8b
Animeology <joseph.nguyen19@yahoo.com>
2023-05-12 19:35:34 -0500

    Double Values for Calculate Rectangle Lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        CalculateTemperature();
+        CalculateRectangle();
+        // CalculateTemperature();
         // Reverse();
         // Calculator();
         // CalculateShapeFromRadius();
@@ -11,6 +12,22 @@
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void CalculateRectangle()
+    {
+        Console.WriteLine("Input two numbers to get the Perimeter, Area, and the Diagonal of the Rectangle");
+
+        double x = Convert.ToDouble(Console.ReadLine());
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        double perimeter = (x * 2) + (y * 2);
+        double area = (x * y);
+        double diagonal = Convert.ToDouble(Math.Sqrt((x * x) + (y * y)));
+
+        Console.WriteLine("Perimeter = {0}", perimeter);
+        Console.WriteLine("Area= {0}", area);
+        Console.WriteLine("Diagonal = {0}", diagonal);
     }
 
     static void CalculateTemperature()
