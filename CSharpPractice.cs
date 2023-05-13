4262c67d6a6ad6b8875e61cd010fa26e44027862
Animeology <joseph.nguyen19@yahoo.com>
2023-05-13 14:27:59 -0500

    Multiply 3 factors Lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        SquaredNumber();
+        MultiplyThreeFactors();
+        // SquaredNumber();
         // RandomGenerator();
         // MultiplicationTable();
         // CalculateTimeDistance();
@@ -16,6 +17,19 @@
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void MultiplyThreeFactors()
+    {
+        Console.WriteLine("Input 3 numbers and it would multiple all three numbers for the result");
+
+        int firstNum = Convert.ToInt32(Console.ReadLine());
+        int secondNum = Convert.ToInt32(Console.ReadLine());
+        int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+        int result = firstNum * secondNum * thirdNum;
+
+        Console.WriteLine("{0}", result);
     }
 
     static void SquaredNumber()
