09bdaa81b0e42a1304384141034b200f4d014598
Animeology <joseph.nguyen19@yahoo.com>
2023-05-12 19:07:23 -0500

    Calculate Shape Lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -1,14 +1,27 @@
-﻿using System;
-
-class CSharpPractice
+﻿class CSharpPractice
 {
     static void Main(string[] args)
     {
-        Comment();
+        CalculateShapeFromRadius();
+        // Comment();
         // StoreUserInput();
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void CalculateShapeFromRadius()
+    {
+        Console.WriteLine("Input a radius of your choice to calculate the Surface and Volume of the shape: ");
+
+        float radius = Convert.ToSingle(Console.ReadLine());
+        float pi = 3.14159265359f;
+
+        float surface = 4 * pi * (radius * radius);
+        float volume = 4 / 3 * pi * (radius * radius * radius);
+
+        Console.WriteLine("Surface: " + surface);
+        Console.WriteLine("Volume: " + volume);
     }
 
     static void Comment()
