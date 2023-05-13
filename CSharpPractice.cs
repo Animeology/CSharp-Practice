64cc87bcfbc775b87900447c6ba5dd1fa52a4316
Animeology <joseph.nguyen19@yahoo.com>
2023-05-12 19:15:01 -0500

    Calculator Lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -2,12 +2,33 @@
 {
     static void Main(string[] args)
     {
-        CalculateShapeFromRadius();
+        Calculator();
+        // CalculateShapeFromRadius();
         // Comment();
         // StoreUserInput();
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void Calculator()
+    {
+        Console.WriteLine("Input 2 numbers to calculate through all operations: ");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Addition: " + (x + y));
+        Console.WriteLine("Subtraction: " + (x - y));
+        Console.WriteLine("Multiplication: " + (x * y));
+        Console.WriteLine("Division: " + (x / y));
+        Console.WriteLine("Modulus: " + (x % y));
+
+        Console.WriteLine("{0} + {1} = {2}", x, y, x + y);
+        Console.WriteLine("{0} - {1} = {2}", x, y, x - y);
+        Console.WriteLine("{0} * {1} = {2}", x, y, x * y);
+        Console.WriteLine("{0} / {1} = {2}", x, y, x / y);
+        Console.WriteLine("{0} mod {1} = {2}", x, y, x % y);
     }
 
     static void CalculateShapeFromRadius()
