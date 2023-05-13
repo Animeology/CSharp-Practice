d83c793004d1acb9a1bbe47e68179371c1fbeb78
Animeology <joseph.nguyen19@yahoo.com>
2023-05-13 14:02:44 -0500

    Calculate Distance Lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        CalculateRectangle();
+        CalculateTimeDistance();
+        // CalculateRectangle();
         // CalculateTemperature();
         // Reverse();
         // Calculator();
@@ -12,6 +13,26 @@
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void CalculateTimeDistance()
+    {
+        Console.WriteLine("Input the desire distance in meters and the time in hours, minutes, seconds in this order to calculate speed in m/s, km/h, m/h");
+
+        float distance = Convert.ToSingle(Console.ReadLine());
+        float hour = Convert.ToSingle(Console.ReadLine());
+        float minute = Convert.ToSingle(Console.ReadLine());
+        float second = Convert.ToSingle(Console.ReadLine());
+
+        float totalSeconds = (hour * 3600) + (minute * 60) + second;
+
+        float metersPerSec = distance / totalSeconds;
+        float kilometersPerHour = (distance / 1000.0f) / (totalSeconds / 3600.0f);
+        float milesPerHour = kilometersPerHour / 1.609f;
+
+        Console.WriteLine("Speed in meter/sec is {0}", metersPerSec);
+        Console.WriteLine("Speed in km/h is {0}", kilometersPerHour);
+        Console.WriteLine("Speed in miles/h is {0}", milesPerHour);
     }
 
     static void CalculateRectangle()
