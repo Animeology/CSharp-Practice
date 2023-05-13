ad484d57dc25d755caad8a90952b053784bbb1eb
Animeology <joseph.nguyen19@yahoo.com>
2023-05-13 14:18:50 -0500

    Random Generator Lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        MultiplicationTable();
+        RandomGenerator();
+        // MultiplicationTable();
         // CalculateTimeDistance();
         // CalculateRectangle();
         // CalculateTemperature();
@@ -14,6 +15,19 @@
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void RandomGenerator()
+    {
+        Console.WriteLine("Input two numbers and it would select a random number between these two numbers");
+
+        int firstNum = Convert.ToInt32(Console.ReadLine());
+        int secondNum = Convert.ToInt32(Console.ReadLine());
+
+        Random rnd = new Random();
+        int randNum = rnd.Next(firstNum, secondNum);
+
+        Console.WriteLine("The random number between {0} and {1} is {2}", firstNum, secondNum, randNum);
     }
 
     static void MultiplicationTable()
