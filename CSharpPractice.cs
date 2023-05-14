a3373dc9800b0a21d245dae26efe80bb92d3f252
Animeology <joseph.nguyen19@yahoo.com>
2023-05-14 12:17:32 -0500

    priority lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        DivideByZero();
+        Priority();
+        // DivideByZero();
         // DivisionAndModule();
         // MultiplyThreeFactors();
         // SquaredNumber();
@@ -19,6 +20,18 @@
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void Priority()
+    {
+        Console.WriteLine("Input a number and it would print out the results for four different equations");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("-6 + {0} * 5 = {1}", x, -6 + (x * 5));
+        Console.WriteLine("(13 - 2) * {0} = {1}", x, (13 - 2) * x);
+        Console.WriteLine("({0} + -2) * (20 / 10) = {1}", x, (x + -2) * (20 / 10));
+        Console.WriteLine("(12 + {0} / (5 - 4) = {1}", x, (12 + x) / (5 - 4));
     }
 
     static void DivideByZero()
