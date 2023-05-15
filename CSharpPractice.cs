e7e2014d7e0c01d9ef4cabc2ffd521194806e8d7
Animeology <joseph.nguyen19@yahoo.com>
2023-05-15 15:12:55 -0500

    sum of infinite numbers lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -1,8 +1,11 @@
-﻿class CSharpPractice
+﻿using System.Security.Cryptography.X509Certificates;
+
+class CSharpPractice
 {
     static void Main(string[] args)
     {
-        NumberRange();
+        SumOfInfiniteNums();
+        // NumberRange();
         // DoWhileLoop();
         // WhileLoop();
         // GreatestNumber();
@@ -31,6 +34,24 @@
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void SumOfInfiniteNums()
+    {
+        Console.WriteLine("Request a number and it would keep adding it until the number is 0");
+
+        int num; 
+        int sum = 0;
+        
+        do
+        {
+            num = Convert.ToInt32(Console.ReadLine());
+            sum = num + sum;
+
+            Console.WriteLine(sum);
+        }
+        while (num != 0);
+        Console.WriteLine("Finished!");
     }
 
     static void NumberRange()
