cd995cdcb6d9de81776ac0b8e4f6ed26a5e10db8
Animeology <joseph.nguyen19@yahoo.com>
2023-05-15 14:11:44 -0500

    average lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        Priority();
+        Average();
+        // Priority();
         // DivideByZero();
         // DivisionAndModule();
         // MultiplyThreeFactors();
@@ -20,6 +21,20 @@
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void Average()
+    {
+        Console.WriteLine("Input 4 numbers and it will take the average of the 4 numbers.");
+
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int num3 = Convert.ToInt32(Console.ReadLine());
+        int num4 = Convert.ToInt32(Console.ReadLine());
+
+        int average = (num1 + num2 + num3 + num4) / 4;
+
+        Console.WriteLine("The average of these four numbers is: {0}", average);
     }
 
     static void Priority()
