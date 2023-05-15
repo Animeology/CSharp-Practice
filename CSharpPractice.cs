44af259e8395134adce2a6cd28cb40fbbbec76e6
Animeology <joseph.nguyen19@yahoo.com>
2023-05-15 14:26:10 -0500

    rectangle lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        Square();
+        Rectangle();
+        // Square();
         // Equivalent();
         // Average();
         // Priority();
@@ -24,6 +25,25 @@
         // PrintLines();
         // HelloWorld();
     }
+
+    static void Rectangle()
+    {
+        Console.WriteLine("Input the number and the width of the square and it would print out the rectangle of the width and height with the number");
+
+        int num = Convert.ToInt32(Console.ReadLine());
+        int width = Convert.ToInt32(Console.ReadLine());
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Console.Write("{0}", num);
+            }
+            Console.WriteLine();
+        }
+    }
+
 
     static void Square()
     {
