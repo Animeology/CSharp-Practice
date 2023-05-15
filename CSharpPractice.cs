7a7fe3e2513525580a41ee89347371147a298b58
Animeology <joseph.nguyen19@yahoo.com>
2023-05-15 14:23:44 -0500

    square lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        Equivalent();
+        Square();
+        // Equivalent();
         // Average();
         // Priority();
         // DivideByZero();
@@ -22,6 +23,23 @@
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void Square()
+    {
+        Console.WriteLine("Input the number and the width of the square and it would print out the square of the width with the number");
+
+        int num = Convert.ToInt32(Console.ReadLine());
+        int width = Convert.ToInt32(Console.ReadLine());
+
+        for(int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Console.Write("{0}", num);
+            }
+            Console.WriteLine();
+        }
     }
 
     static void Equivalent()
