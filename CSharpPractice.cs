ffb4e6493992d1259ac1166cf21f4e5638d4c6f6
Animeology <joseph.nguyen19@yahoo.com>
2023-05-15 14:15:31 -0500

    equivalent lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        Average();
+        Equivalent();
+        // Average();
         // Priority();
         // DivideByZero();
         // DivisionAndModule();
@@ -21,6 +22,21 @@
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void Equivalent()
+    {
+        Console.WriteLine("Input 3 numbers and it would display the results for the two equations.");
+
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int equation1 = (a + b) * c;
+        int equation2 = a * c + b * c;
+
+        Console.WriteLine("({0}+{1}) * {2} = {3}", a, b, c, equation1);
+        Console.WriteLine("{0}*{2} + {1}*{2} = {3}", a, b, c, equation2);
     }
 
     static void Average()
