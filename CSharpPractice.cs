00df554cba148584c5c167849b09439ba0da1b84
Animeology <joseph.nguyen19@yahoo.com>
2023-05-15 14:52:17 -0500

    Do While Loop lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -2,7 +2,8 @@
 {
     static void Main(string[] args)
     {
-        WhileLoop();
+        DoWhileLoop();
+        // WhileLoop();
         // GreatestNumber();
         // PosOrNeg();
         // SwitchCalculator();
@@ -30,6 +31,21 @@
         // PrintLines();
         // HelloWorld();
     }
+
+    static void DoWhileLoop()
+    {
+        Console.WriteLine("Input a number and it would multiple by 10 and keeps going until the input is 0");
+
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        do
+        {
+            Console.WriteLine(num * 10);
+            num = Convert.ToInt32(Console.ReadLine());
+        }
+        while (num != 0);
+    }
+
 
     static void WhileLoop()
     {
