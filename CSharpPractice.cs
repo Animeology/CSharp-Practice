e1ca8c1477c5fe3181a69715149064866f31cb78
Animeology <joseph.nguyen19@yahoo.com>
2023-05-16 18:04:33 -0500

    letter calculator lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -4,7 +4,8 @@ class CSharpPractice
 {
     static void Main(string[] args)
     {
-        InfiniteMultiplication();
+        CalculateGrade();
+        // InfiniteMultiplication();
         // RepeatingNumber();
         // SumOfInfiniteNums();
         // NumberRange();
@@ -36,6 +37,36 @@ class CSharpPractice
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void CalculateGrade()
+    {
+        Console.WriteLine("Input a number between 0 to 10 and it would give the grade letter");
+
+        int grade = Convert.ToInt32(Console.ReadLine());
+
+        switch (grade)
+        {
+            case 10:
+                Console.WriteLine("Grade Letter: A+");
+                break;
+            case 9:
+                Console.WriteLine("Grade Letter: A");
+                break;
+            case 8:
+            case 7:
+                Console.WriteLine("Grade Letter: B");
+                break;
+            case 6:
+                Console.WriteLine("Grade Letter: C");
+                break;
+            case 5:
+                Console.WriteLine("Grade Letter: E");
+                break;
+            default:
+                Console.WriteLine("Grade Letter: F");
+                break;
+        }
     }
 
     static void InfiniteMultiplication()
