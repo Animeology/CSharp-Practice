447b082e06a4e47ffcde2e9fc034bd18fba87620
Animeology <joseph.nguyen19@yahoo.com>
2023-05-17 14:13:09 -0500

    product lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -4,7 +4,8 @@ class CSharpPractice
 {
     static void Main(string[] args)
     {
-        AbsoluteValue();
+        Product();
+        // AbsoluteValue();
         // Digits();
         // CalculateGrade();
         // InfiniteMultiplication();
@@ -39,6 +40,23 @@ class CSharpPractice
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void Product()
+    {
+        Console.WriteLine("Input two numbers and the product of the two numbers is displayed (not using multiplication)");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        int total = 0;
+
+        for(int i = 0; i < y; i++)
+        {
+            total += x;
+        }
+
+        Console.WriteLine("{0}x{1}={2}", x, y, total);
     }
 
     static void AbsoluteValue()
