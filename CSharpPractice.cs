858d148c80c10a9874a363ba74a3cf695b7f5837
Animeology <joseph.nguyen19@yahoo.com>
2023-05-17 14:32:21 -0500

    odd or even descending lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -4,7 +4,8 @@ class CSharpPractice
 {
     static void Main(string[] args)
     {
-        PrimeNumber();
+        OddDescending();
+        // PrimeNumber();
         // Product();
         // AbsoluteValue();
         // Digits();
@@ -41,6 +42,45 @@ class CSharpPractice
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void OddDescending()
+    {
+        Console.WriteLine("Input two numbers which will be a range of numbers");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Odd or Even?");
+        string choice = Console.ReadLine();
+
+        switch(choice)
+        {
+            case "odd":
+            case "Odd":
+                for(int i = x; i >= y; i--)
+                {
+                    if(i % 2 != 0)
+                    {
+                        Console.Write("{0}, ", i);
+                    }
+                }
+                break;
+            case "even":
+            case "Even":
+                for (int i = x; i >= y; i--)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write("{0}, ", i);
+                    }
+                }
+                break;
+            default:
+                Console.WriteLine("Not a valid choice");
+                break;
+        }
+
     }
 
     static void PrimeNumber()
