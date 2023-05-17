c846ba530ec075eaf9c3aa755781915ea64ca872
Animeology <joseph.nguyen19@yahoo.com>
2023-05-17 14:07:54 -0500

    absolute value lesson

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -4,7 +4,8 @@ class CSharpPractice
 {
     static void Main(string[] args)
     {
-        Digits();
+        AbsoluteValue();
+        // Digits();
         // CalculateGrade();
         // InfiniteMultiplication();
         // RepeatingNumber();
@@ -38,6 +39,23 @@ class CSharpPractice
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void AbsoluteValue()
+    {
+        Console.WriteLine("Input a number and the absolute value of the number is printed");
+
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if(num > 0)
+        {
+            Console.WriteLine("Absolute Value is {0}", num);
+        }
+        else
+        {
+            num = -num;
+            Console.WriteLine("Absolute Value is {0}", num);
+        }
     }
     
     static void Digits()
