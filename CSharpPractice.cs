311ad4f212fe9bc10a023922ad20650081a45020
Animeology <joseph.nguyen19@yahoo.com>
2023-05-18 18:59:59 -0500

    Check for even using conditional operator

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -4,7 +4,8 @@ class CSharpPractice
 {
     static void Main(string[] args)
     {
-        ExceptionsChecker();
+        CheckForEven();
+        // ExceptionsChecker();
         // VowelOrNot();
         // OddOrEvenDescending();
         // PrimeNumber();
@@ -44,6 +45,17 @@ class CSharpPractice
         // ReadUserInput();
         // PrintLines();
         // HelloWorld();
+    }
+
+    static void CheckForEven()
+    {
+        Console.WriteLine("Input two numbers and it would check if both are even or not");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        bool isEven = ((x % 2 == 0) && (y % 2 == 0)) ? true : false;
+        Console.WriteLine(isEven);
     }
 
     static void ExceptionsChecker()
