3fd29285e3ef0c8241b6488d20ff856d954b2692
Animeology <joseph.nguyen19@yahoo.com>
2023-07-10 19:25:17 -0500

    Create CalculateRadius.cs

---
+++ DataTypesA/CalculateRadius.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharp_Practice.DataTypesA
+{
+    public class CalculateRadius
+    {
+        public static void CalculateShapeFromRadius()
+        {
+            Console.WriteLine("Input a radius of your choice to calculate the Surface and Volume of the shape:");
+
+            float radius = Convert.ToSingle(Console.ReadLine());
+            float pi = 3.14159265359f;
+
+            float surface = 4 * pi * (radius * radius);
+            float volume = 4 / 3 * pi * (radius * radius * radius);
+
+            Console.WriteLine("Surface: " + surface);
+            Console.WriteLine("Volume: " + volume);
+        }
+    }
+}
