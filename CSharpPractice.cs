d2d5d717f06c3df0ac17abfff04a9897036c4263
Animeology <joseph.nguyen19@yahoo.com>
2023-07-11 14:07:50 -0500

    Update CSharpPractice.cs

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -32,6 +32,7 @@
             ZeroDivision.DivideByZero();
             PriorityOrder.Priority();
             AverageSum.Average();
+            EquivalentOperations.Equivalent();
         }
 
 
@@ -623,21 +624,6 @@
                 }
                 Console.WriteLine();
             }
-        }
-
-        static void Equivalent()
-        {
-            Console.WriteLine("Input 3 numbers and it would display the results for the two equations.");
-
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            int c = Convert.ToInt32(Console.ReadLine());
-
-            int equation1 = (a + b) * c;
-            int equation2 = a * c + b * c;
-
-            Console.WriteLine("({0}+{1}) * {2} = {3}", a, b, c, equation1);
-            Console.WriteLine("{0}*{2} + {1}*{2} = {3}", a, b, c, equation2);
         }
 
     }
