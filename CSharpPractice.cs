f7ad7d317663a71c40224bbd848fe60684b4af0c
Animeology <joseph.nguyen19@yahoo.com>
2023-07-11 14:02:10 -0500

    Update CSharpPractice.cs

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -28,6 +28,7 @@
             // Arithmetic
             SumSquare.SquaredNumber();
             MultiplicationOfThreeFactors.MultiplyThreeFactors();
+            Division.DivisionAndModule();
         }
 
 
@@ -678,18 +679,6 @@
                 Console.WriteLine("Cannot Divide By Zero");
             }
         }
-
-        static void DivisionAndModule()
-        {
-            Console.WriteLine("Input two integers to get the division and rest of the division of the result");
-
-            int x = Convert.ToInt32(Console.ReadLine());
-            int y = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("{0}", x / y);
-            Console.WriteLine("{0}", x % y);
-        }
-
 
     }
 }
