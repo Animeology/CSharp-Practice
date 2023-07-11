4540c8f04ab4f9426d72449b541ca0e4d0a55dac
Animeology <joseph.nguyen19@yahoo.com>
2023-07-10 19:32:54 -0500

    Update CSharpPractice.cs

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -19,6 +19,8 @@ class CSharpPractice
         Temperature.CalculateTemperature();
         Rectangle.CalculateRectangle();
         TimeDistance.CalculateTimeDistance();
+        MultiplicationTable.CreateMultiplicationTable();
+
     }
 
 
@@ -717,15 +719,4 @@ class CSharpPractice
         Console.WriteLine("The random number between {0} and {1} is {2}", firstNum, secondNum, randNum);
     }
 
-    static void MultiplicationTable()
-    {
-        Console.WriteLine("Input the desire number to go through the multiplicatoin table to 10");
-
-        int num = Convert.ToInt32(Console.ReadLine());
-
-        for (int i = 1; i <= 10; i++)
-        {
-            Console.WriteLine("{0}x{1}={2}", num, i, num * i);
-        }
-    }
 }
