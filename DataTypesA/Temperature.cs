4e3e49806c6bd8e362cc7d3de43798545a7fbcc8
Animeology <joseph.nguyen19@yahoo.com>
2023-07-10 19:27:54 -0500

    Create Temperature.cs

---
+++ DataTypesA/Temperature.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharp_Practice.DataTypesA
+{
+    public class Temperature
+    {
+        public static void CalculateTemperature()
+        {
+            Console.WriteLine("Input your degree in Celsius to convert to Kelvin and Fahrenheit:");
+
+            int celsius = Convert.ToInt32(Console.ReadLine());
+
+            int kelvin = celsius + 273;
+            int fahrenheit = celsius * 18 / 10 + 32;
+
+            Console.WriteLine("Kelvin: " + kelvin + " degrees");
+            Console.WriteLine("Fahrenheit: " + fahrenheit + " degrees");
+
+            Console.WriteLine("Kelvin = {0}", kelvin);
+            Console.WriteLine("Fahrenheit = {0}", fahrenheit);
+        }
+    }
+}
