6a910ff7bb4a5d4b6776dd9f90351b8f957b6b5f
Animeology <joseph.nguyen19@yahoo.com>
2023-07-11 14:12:26 -0500

    Update CSharpPractice.cs

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -37,6 +37,7 @@
 
             // Geometry A
             SquareShape.Square();
+            RectangleShape.Rectangle();
         }
 
 
@@ -595,23 +596,6 @@
 
         }
 
-        static void RectangleShape()
-        {
-            Console.WriteLine("Input the number and the width of the square and it would print out the rectangle of the width and height with the number");
-
-            int num = Convert.ToInt32(Console.ReadLine());
-            int width = Convert.ToInt32(Console.ReadLine());
-            int height = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    Console.Write("{0}", num);
-                }
-                Console.WriteLine();
-            }
-        }
-
+        
     }
 }
