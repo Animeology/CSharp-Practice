cf238179d2b7508a9dc2aa8cd7ad725c57ae1608
Animeology <joseph.nguyen19@yahoo.com>
2023-07-11 14:00:35 -0500

    Create MultiplicationOfThreeFactors.cs

---
+++ Arithmetic/MultiplicationOfThreeFactors.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp_Practice.Arithmetic
+{
+    public class MultiplicationOfThreeFactors
+    {
+        public static void MultiplyThreeFactors()
+        {
+            Console.WriteLine("Input 3 numbers and it would multiple all three numbers for the result");
+
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+            int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+            int result = firstNum * secondNum * thirdNum;
+
+            Console.WriteLine("{0}", result);
+        }
+    }
+}
