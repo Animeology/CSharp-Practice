89150581c9d8ec615da30b55d4128a3845eaea28
Animeology <joseph.nguyen19@yahoo.com>
2023-07-10 19:13:18 -0500

    Create PrintLines.cs

---
+++ Introduction/PrintLines.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharp_Practice.Introduction
+{
+    public class PrintLines
+    {
+        public static void PrintingLines()
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Joseph");
+        }
+
+    }
+}
