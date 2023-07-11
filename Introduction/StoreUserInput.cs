76f371678ab4fdf45d90391f2f6e74afb4ea053f
Animeology <joseph.nguyen19@yahoo.com>
2023-07-10 19:16:28 -0500

    Create StoreUserInput.cs

---
+++ Introduction/StoreUserInput.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharp_Practice.Introduction
+{
+    public class StoreUserInput
+    {
+        public static void StoringUserInput()
+        {
+            Console.WriteLine("What is your name?");
+            string x = Console.ReadLine()!;
+            Console.WriteLine("Nice to meet you, " + x);
+        }
+    }
+}
