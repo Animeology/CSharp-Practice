30263752d8b14acf36250962519859ade11f6fef
Animeology <joseph.nguyen19@yahoo.com>
2023-07-11 14:00:37 -0500

    Update CSharpPractice.cs

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -27,6 +27,7 @@
 
             // Arithmetic
             SumSquare.SquaredNumber();
+            MultiplicationOfThreeFactors.MultiplyThreeFactors();
         }
 
 
@@ -689,19 +690,6 @@
             Console.WriteLine("{0}", x % y);
         }
 
-        static void MultiplyThreeFactors()
-        {
-            Console.WriteLine("Input 3 numbers and it would multiple all three numbers for the result");
-
-            int firstNum = Convert.ToInt32(Console.ReadLine());
-            int secondNum = Convert.ToInt32(Console.ReadLine());
-            int thirdNum = Convert.ToInt32(Console.ReadLine());
-
-            int result = firstNum * secondNum * thirdNum;
-
-            Console.WriteLine("{0}", result);
-        }
 
     }
-
 }
