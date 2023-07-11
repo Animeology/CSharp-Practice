7d55bccbac54b709c387dd80a45d401e23c7e89e
Animeology <joseph.nguyen19@yahoo.com>
2023-07-11 14:02:08 -0500

    Create Division.cs

---
+++ Arithmetic/Division.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharp_Practice.Arithmetic
+{
+    public class Division
+    {
+        public static void DivisionAndModule()
+        {
+            Console.WriteLine("Input two integers to get the division and rest of the division of the result");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0}", x / y);
+            Console.WriteLine("{0}", x % y);
+        }
+    }
+}
