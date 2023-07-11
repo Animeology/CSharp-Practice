95080f2b3b669625a4f7fa9838b77520903cd19d
Animeology <joseph.nguyen19@yahoo.com>
2023-07-11 14:12:25 -0500

    Create RectangleShape.cs

---
+++ GeometryA/RectangleShape.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharp_Practice.GeometryA
+{
+    public class RectangleShape
+    {
+        public static void Rectangle()
+        {
+            Console.WriteLine("Input the number and the width of the square and it would print out the rectangle of the width and height with the number");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int width = Convert.ToInt32(Console.ReadLine());
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write("{0}", num);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
