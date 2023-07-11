54c43baee52cc01587c49472260edd620135cd22
Animeology <joseph.nguyen19@yahoo.com>
2023-07-10 19:31:41 -0500

    Update CSharpPractice.cs

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -18,6 +18,7 @@ class CSharpPractice
         Reverse.ReverseCharacters();
         Temperature.CalculateTemperature();
         Rectangle.CalculateRectangle();
+        TimeDistance.CalculateTimeDistance();
     }
 
 
@@ -727,25 +728,4 @@ class CSharpPractice
             Console.WriteLine("{0}x{1}={2}", num, i, num * i);
         }
     }
-
-    static void CalculateTimeDistance()
-    {
-        Console.WriteLine("Input the desire distance in meters and the time in hours, minutes, seconds in this order to calculate speed in m/s, km/h, m/h");
-
-        float distance = Convert.ToSingle(Console.ReadLine());
-        float hour = Convert.ToSingle(Console.ReadLine());
-        float minute = Convert.ToSingle(Console.ReadLine());
-        float second = Convert.ToSingle(Console.ReadLine());
-
-        float totalSeconds = (hour * 3600) + (minute * 60) + second;
-
-        float metersPerSec = distance / totalSeconds;
-        float kilometersPerHour = (distance / 1000.0f) / (totalSeconds / 3600.0f);
-        float milesPerHour = kilometersPerHour / 1.609f;
-
-        Console.WriteLine("Speed in meter/sec is {0}", metersPerSec);
-        Console.WriteLine("Speed in km/h is {0}", kilometersPerHour);
-        Console.WriteLine("Speed in miles/h is {0}", milesPerHour);
-    }
-
 }
