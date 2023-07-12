4b4e8f35687f27de5529551eb51ca7076c8219e6
Animeology <joseph.nguyen19@yahoo.com>
2023-07-11 19:01:13 -0500

    Update CSharpPractice.cs

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -42,7 +42,7 @@
 
             // Flow Controls A
             TheIFCalculator.IfCalculator();
-
+            TheSwitchCalculator.SwitchCalculator();
         }
 
 
@@ -535,37 +535,6 @@
             {
                 Console.WriteLine("Negative");
             }
-        }
-
-        static void SwitchCalculator()
-        {
-            Console.WriteLine("Input two numbers and operation and it would calculate for you");
-
-            int x = Convert.ToInt32(Console.ReadLine());
-            char op = Convert.ToChar(Console.Read());
-            int y = Convert.ToInt32(Console.ReadLine());
-
-            switch (op)
-            {
-                case '+':
-                    Console.WriteLine("{0}+{1}={2}", x, y, x + y);
-                    break;
-                case '-':
-                    Console.WriteLine("{0}-{1}={2}", x, y, x - y);
-                    break;
-                case 'x':
-                case '*':
-                    Console.WriteLine("{0}x{1}={2}", x, y, x * y);
-                    break;
-                case '/':
-                    Console.WriteLine("{0}/{1}={2}", x, y, x / y);
-                    break;
-                default:
-                    Console.WriteLine("Unrecognized character");
-                    break;
-            }
-        }
-
-        
+        }       
     }
 }
