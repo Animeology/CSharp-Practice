04ec0c30fb76ca4ea2c9c70e2e616a2259a444b7
Animeology <joseph.nguyen19@yahoo.com>
2023-07-11 19:19:30 -0500

    Create AbsoluteValue.cs

---
+++ FlowControlsC/AbsoluteValue.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharp_Practice.FlowControlsC
+{
+    public class AbsoluteValue
+    {
+        public static void DisplayAbsoluteValue()
+        {
+            Console.WriteLine("Input a number and the absolute value of the number is printed");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0)
+            {
+                Console.WriteLine("Absolute Value is {0}", num);
+            }
+            else
+            {
+                num = -num;
+                Console.WriteLine("Absolute Value is {0}", num);
+            }
+        }
+    }
+}
