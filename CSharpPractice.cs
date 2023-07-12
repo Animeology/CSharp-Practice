4a1de3bdc4b5a8ef923066744bd0ebed63d096fa
Animeology <joseph.nguyen19@yahoo.com>
2023-07-11 19:02:59 -0500

    Update CSharpPractice.cs

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -43,6 +43,8 @@
             // Flow Controls A
             TheIFCalculator.IfCalculator();
             TheSwitchCalculator.SwitchCalculator();
+            PositiveAndNegative.PosOrNeg();
+
         }
 
 
@@ -521,20 +523,5 @@
 
         }
 
-        static void PosOrNeg()
-        {
-            Console.WriteLine("Input a number and it will tell if it is positive or negative.");
-
-            int num = Convert.ToInt32(Console.ReadLine());
-
-            if (num > 0)
-            {
-                Console.WriteLine("Positive");
-            }
-            else
-            {
-                Console.WriteLine("Negative");
-            }
-        }       
     }
 }
