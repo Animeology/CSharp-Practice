6041fc4c5719489d8c419d853ee97fe7d06418ff
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 08:30:52 -0500

    Create NumberCheckerException.cs

---
+++ DataTypesB/NumberCheckerException.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharp_Practice.DataTypesB
+{
+    public class NumberCheckerException
+    {
+        public static void ExceptionsChecker()
+        {
+            Console.WriteLine("Input a word and it would detect if it is a number or not");
+
+            string line = Convert.ToString(Console.ReadLine()!);
+
+            try
+            {
+                Convert.ToInt32(line);
+                Console.WriteLine("It is a number.");
+            }
+            catch
+            {
+                Console.WriteLine("It is not a number.");
+            }
+        }
+    }
+}
