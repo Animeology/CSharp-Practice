954a9faa14d14715e3b353d61f9fba9ffb47c459
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 08:54:34 -0500

    Create FunctionWithReturnValue.cs

---
+++ FunctionsA/FunctionWithReturnValue.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_Practice.FunctionsA
+{
+    public class FunctionWithReturnValue
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
