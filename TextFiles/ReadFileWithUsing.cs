7e16e27f13469b0d4fdd98c60f258edb61eaaa1d
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 09:03:21 -0500

    Create ReadFileWithUsing.cs

---
+++ TextFiles/ReadFileWithUsing.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharp_Practice.TextFiles
+{
+    public class ReadFileWithUsing
+    {
+        public static void UsingReadFile()
+        {
+            Console.WriteLine("Read input.txt file with 'using' keyword");
+
+            string fileName = "input.txt";
+            string line;
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                line = sr.ReadLine()!;
+
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine()!;
+                }
+                sr.Close();
+            }
+        }
+    }
+}
