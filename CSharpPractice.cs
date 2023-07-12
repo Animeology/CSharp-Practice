50cc80cdb14b6f4066135b504eb0abc59198dc34
Animeology <joseph.nguyen19@yahoo.com>
2023-07-12 09:03:47 -0500

    Finish refactor

--- CSharpPractice.cs
+++ CSharpPractice.cs
@@ -9,6 +9,7 @@
     using FlowControlsC;
     using DataTypesB;
     using FunctionsA;
+    using CSharp_Practice.TextFiles;
 
     class CSharpPractice
     {
@@ -77,114 +78,13 @@
             GreetingAndFarewell.Farewell();
             FunctionWithParameters.GreetingAndFarewell("Joseph");
             FunctionWithReturnValue.Add(10, 15);
+
+            // Text File
+            StreamReaderReadFile.ReadFile();
+            ReadFileWithUsing.UsingReadFile();
+            ReadFileWithFileKeyword.FileRead();
+            WriteFile.WriteFileStreamWriter();
+            AppendFile.AppendFileUsingFile();
         }
-
-
-        static void AppendFile()
-        {
-            Console.WriteLine("Write whatever and it will add to the current 'output.txt' file if created already");
-
-            string fileName = "output.txt";
-
-            if (File.Exists(fileName))
-            {
-                using (StreamWriter file = File.AppendText(fileName))
-                {
-                    string line;
-
-                    do
-                    {
-                        line = Console.ReadLine();
-
-                        if (line.Length != 0)
-                        {
-                            file.WriteLine(line);
-
-                        }
-                    }
-                    while (line.Length != 0);
-                }
-            }
-            else
-            {
-                WriteFile();
-            }
-
-        }
-
-        static void WriteFile()
-        {
-            Console.WriteLine("Write whatever you want and it will be write into the 'output.txt' file");
-
-            string fileName = "output.txt";
-
-            // Using method
-            using (StreamWriter file = File.CreateText(fileName))
-            {
-                string line;
-
-                do
-                {
-                    line = Console.ReadLine();
-
-                    if (line.Length != 0)
-                    {
-                        file.WriteLine(line);
-                    }
-                }
-                while (line.Length != 0);
-            }
-
-        }
-
-        static void FileRead()
-        {
-            Console.WriteLine("Read input.txt file with using the File keyword");
-
-            string fileName = "input.txt";
-            string readText = File.ReadAllText(fileName);
-
-
-            Console.WriteLine(readText);
-        }
-
-        static void UsingReadFile()
-        {
-            Console.WriteLine("Read input.txt file with 'using' keyword");
-
-            string fileName = "input.txt";
-            string line;
-
-            using (StreamReader sr = new StreamReader(fileName))
-            {
-                line = sr.ReadLine();
-
-                while (line != null)
-                {
-                    Console.WriteLine(line);
-                    line = sr.ReadLine();
-                }
-                sr.Close();
-            }
-        }
-
-        static void ReadFile()
-        {
-            Console.WriteLine("Read the contents of input.txt text file");
-
-            string fileName = "input.txt";
-            string line;
-
-            StreamReader sr = new StreamReader(fileName);
-            line = sr.ReadLine();
-
-            while (line != null)
-            {
-                Console.WriteLine(line);
-                line = sr.ReadLine();
-            }
-            sr.Close();
-        }
-
     }
 }
